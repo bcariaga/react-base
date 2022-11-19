f3c002e41ed05d670f93e0457e7e574e51894cb1
Braian Cariaga <cariaga.bh@gmail.com>
2022-11-19 16:02:08 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/Index.tsx',
+  devtool: 'eval-source-map',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './dist'),
+    },
+  },
+}
